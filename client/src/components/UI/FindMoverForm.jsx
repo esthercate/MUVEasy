b03c09978a60e9de5c9b85118375e70847cb73a4
catherine <vuthi.esther@gmail.com>
2022-07-30 20:46:07 +0300

    added filter form

--- client/src/components/UI/FindMoverForm.jsx
+++ client/src/components/UI/FindMoverForm.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../../styles/find-mover-form.css'
+import { Form, FormGroup } from "reactstrap";
 
 const FindMoverForm = () => {
   return (
-    <div>FindMoverForm</div>
-  )
+    <Form className="form">
+      <div className=" d-flex align-items-center justify-content-between flex-wrap">
+        <FormGroup className="form__group">
+          <input type="text" placeholder="Location" required />
+        </FormGroup>
+
+        <FormGroup className="form__group">
+          <input type="text" placeholder="Min Charges" required />
+        </FormGroup>
+
+        <FormGroup className="form__group">
+          <input type="text" placeholder="Max Charges" required />
+        </FormGroup>
+      </div>
+    </Form>
+  );
 }
 
 export default FindMoverForm
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSlider from "../components/UI/HeroSlider";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
+import FindMoverForm from "../components/UI/FindMoverForm";
 
 const Home = () => {
   return (
@@ -11,7 +12,19 @@ const Home = () => {
         <HeroSlider />
 
         <div className="hero_form">
-          <Container></Container>
+          <Container>
+            <Row className="form_row">
+              <Col lg="4" md="4">
+                <div className="find_movers-left">
+                  <h2>Find a mover here</h2>
+                </div>
+              </Col>
+
+              <Col lg="8" md="8" sm="12">
+                <FindMoverForm />
+              </Col>
+            </Row>
+          </Container>
         </div>
       </section>
     </Helmet>
