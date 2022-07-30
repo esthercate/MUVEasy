cf6f6b036e5cc9e6badb981f338b43f29b02a888
catherine <vuthi.esther@gmail.com>
2022-07-30 11:14:34 +0300

    complete navigation section

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from 'react-router-dom';
+import '../../styles/header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div className="header_top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="header_top_left">
+                <span className="logo">
+                  <i class="ri-truck-line"></i>
+                </span>
+                <span className="site_name">MUVEasy</span>
+              </div>
+            </Col>
 
-export default Header
+            <Col lg="6" md="6" sm="6">
+              <div className="header_top_right d-flex align-items-center justify-content-end gap-3">
+                <Link to="#" className="d-flex align-items-center gap-1">
+                   Login
+                </Link>
+                <Link to="#" className="d-flex align-items-center gap-1">
+                   Register
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
