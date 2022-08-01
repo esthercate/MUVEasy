27e56e5aef7d2d5a8ccd190432e5d32d6d90b227
catherine <vuthi.esther@gmail.com>
2022-08-01 12:09:32 +0300

    added users schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
 
     def create 
-        
+
+
+    end
+
+    def user_params 
+        params.permit(:username, :password, :password_confirmation, :role)
     end
 
 end
