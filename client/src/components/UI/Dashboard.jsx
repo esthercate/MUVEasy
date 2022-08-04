f1192b1b00d7b5b9ea69e45fa9cda5eb17b6cb7e
catherine <vuthi.esther@gmail.com>
2022-08-04 23:06:09 +0300

    dashboard background

--- client/src/components/UI/Dashboard.jsx
+++ client/src/components/UI/Dashboard.jsx
@@ -5,7 +5,6 @@ import ProfileForm from "./ProfileForm";
 import "../../styles/dashboard.css";
 import RequestTable from "./RequestTable";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
 const Dashboard = ({ user, setUser }) => {
@@ -26,92 +25,95 @@ const Dashboard = ({ user, setUser }) => {
   }
 
   return (
-    <div className="my-dashboard">
-      <Container>
-        {/*================== left menu ================ */}
-        <Row>
-          <Col lg="3" className="left-col p-5">
-            <div className="avatar d-flex">
-              <div className="user_image"></div>
-              <div className="user_details">
-                <h6>{user ? user.username : " "}</h6>
-                <p>@{user ? user.username : " "}</p>
+    <div className="outer">
+      <div className="outer_container"></div>
+      <div className="my-dashboard">
+        <Container>
+          {/*================== left menu ================ */}
+          <Row>
+            <Col lg="3" className="left-col p-5">
+              <div className="avatar d-flex">
+                <div className="user_image"></div>
+                <div className="user_details">
+                  <h6>{user ? user.username : " "}</h6>
+                  <p>@{user ? user.username : " "}</p>
+                </div>
               </div>
-            </div>
 
-            <div className="dashboard_links mt-3 mb-5">
-              <div className="links mb-3">
-                <Link to="#">
+              <div className="dashboard_links mt-3 mb-5">
+                <div className="links mb-3">
+                  <Link to="#">
+                    {" "}
+                    <i class="ri-home-7-line"></i> <span>General</span>
+                  </Link>
+                </div>
+                <div className="links mb-3">
+                  <Link to="" onClick={handleShow}>
+                    {" "}
+                    <i class="ri-lock-line"></i> <span>Create Profile</span>
+                  </Link>
+
+                  {/*================== profile form modal ================ */}
+
+                  <Modal size="lg" show={show} onHide={handleClose}>
+                    <Modal.Header closeButton></Modal.Header>
+                    <Modal.Body>
+                      <div className="profileform hidden">
+                        <ProfileForm user={user} />
+                      </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </div>
+                <div className="links mb-3">
+                  <Link to="#">
+                    {" "}
+                    <i class="ri-message-line"></i> <span>Messages</span>
+                  </Link>
+                </div>
+                <div className="links mb-3">
+                  <Link to="#">
+                    {" "}
+                    <i class="ri-mail-download-line"></i>{" "}
+                    <span>Pending Requests</span>
+                  </Link>
+                </div>
+                <div className="links mb-3">
+                  <Link to="#">
+                    {" "}
+                    <i class="ri-check-double-line"></i>{" "}
+                    <span>Completed Jobs</span>
+                  </Link>
+                </div>
+              </div>
+
+              <div className="logout">
+                <Link to="#" onClick={handleLogout}>
                   {" "}
-                  <i class="ri-home-7-line"></i> <span>General</span>
+                  <i class="ri-logout-box-r-line"></i> <span>Log out</span>
                 </Link>
               </div>
-              <div className="links mb-3">
-                <Link to="" onClick={handleShow}>
-                  {" "}
-                  <i class="ri-lock-line"></i> <span>Create Profile</span>
-                </Link>
+            </Col>
 
-                {/*================== profile form modal ================ */}
-
-                <Modal size="lg" show={show} onHide={handleClose}>
-                  <Modal.Header closeButton></Modal.Header>
-                  <Modal.Body>
-                    <div className="profileform hidden">
-                      <ProfileForm user={user} />
-                    </div>
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                      Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                      Save Changes
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
+            <Col lg="9" className="right-col p-4">
+              <div className="dashboard-title mb-4">
+                <h2>Welcome {user ? user.username : " "}</h2>
               </div>
-              <div className="links mb-3">
-                <Link to="#">
-                  {" "}
-                  <i class="ri-message-line"></i> <span>Messages</span>
-                </Link>
-              </div>
-              <div className="links mb-3">
-                <Link to="#">
-                  {" "}
-                  <i class="ri-mail-download-line"></i>{" "}
-                  <span>Pending Requests</span>
-                </Link>
-              </div>
-              <div className="links mb-3">
-                <Link to="#">
-                  {" "}
-                  <i class="ri-check-double-line"></i>{" "}
-                  <span>Completed Jobs</span>
-                </Link>
-              </div>
-            </div>
 
-            <div className="logout">
-              <Link to="#" onClick={handleLogout}>
-                {" "}
-                <i class="ri-logout-box-r-line"></i> <span>Log out</span>
-              </Link>
-            </div>
-          </Col>
-
-          <Col lg="9" className="right-col p-4">
-            <div className="dashboard-title mb-4">
-              <h2>Welcome {user ? user.username : " "}</h2>
-            </div>
-
-            <div>
-              <RequestTable />
-            </div>
-          </Col>
-        </Row>
-      </Container>
+              <div>
+                <RequestTable />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
